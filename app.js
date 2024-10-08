86067bff4fd40350fc39fded25d9ffaf12e6593e
Abhaya Bikram Shahi <abhayabikramshahiofficial@gmail.com>
2024-10-08 11:52:29 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+document.getElementById("call"), function () {
+    document.getElementById("call").addEventListener("click", () => {
+        window.location.href = "tel:9808370638"
+    })
+};
