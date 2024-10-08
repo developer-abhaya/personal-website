800ae94f9e87b248a14dcba7094a7a5b28dc2acb
Abhaya Bikram Shahi <abhayabikramshahiofficial@gmail.com>
2024-10-08 11:56:31 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-document.addEventListener("DOMContentLoaded", () => {
+ // Wait for the DOM to load
+ document.addEventListener("DOMContentLoaded", () => {
+    // Add click event listener to the button
     document.getElementById("call").addEventListener("click", () => {
+        // Initiate a phone call with the specified number
         window.location.href = "tel:9808370638";
     });
 });
